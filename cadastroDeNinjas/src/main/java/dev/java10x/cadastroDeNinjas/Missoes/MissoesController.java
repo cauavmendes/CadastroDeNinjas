309e679874c88e6ae244c92abba01508e6708681
Cauã Mendes <caua11mendes@gmail.com>
2026-04-14 15:18:37 -0300

    feat: Cria as rotas do Controller com o mock

--- cadastroDeNinjas/src/main/java/dev/java10x/cadastroDeNinjas/Missoes/MissoesController.java
+++ cadastroDeNinjas/src/main/java/dev/java10x/cadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,32 @@
 package dev.java10x.cadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 public class MissoesController {
+
+    // POST -- Mandar uma requisição para criar as missões
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "Missao criada com sucesso";
+    }
+
+    // PUT -- Mandar uma requisição parar alterar as missões
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missao alterada";
+    }
+
+    // DELETE -- Mandar uma requisição para deletar as missões
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Missao deletada";
+    }
+
+    // GET -- Mandar uma requisição para mostrar as missões
+    @GetMapping("/listar")
+    public String listarMissao(){
+        return "Missao listada";
+    }
 }
